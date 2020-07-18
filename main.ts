e5beb83657ce2e49807fa961777031df26318711
lanesty29 <68239154+lanesty29@users.noreply.github.com>
2020-07-18 13:37:42 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+radio.setGroup(99)
+basic.forever(function () {
+    radio.sendValue("x", input.acceleration(Dimension.X))
+    radio.sendValue("y", input.acceleration(Dimension.Y))
+    radio.sendValue("z", input.acceleration(Dimension.Z))
+})
